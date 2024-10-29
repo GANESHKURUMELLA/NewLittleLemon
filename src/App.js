70e71d60a5c9a4691daaa5f43fcee5a40f83a5f8
GANESH <ganesh@GANESHs-Air.lan>
2024-10-29 15:36:43 +0530

    new update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    < div className = "App">
+
+    <header>
+      Little Lemon
+    </header>
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Book a table</li>
+        <li>Order online</li>
+        <li>Contact us</li>
+      </ul>
+    </nav>
+    <main>
+
+    </main>
+    <footer>
+      All rights reserved
+    </footer>
     </div>
   );
 }
